84660bedf182f4365120dab3a45cc3f80adc44a3
blackmax <blackmax1886@gmail.com>
2023-10-23 12:06:38 +0900

    Feat: team page

---
+++ app/about/team/page.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+import React from 'react'
+
+const TeamPage = () => {
+  return (
+    <div>
+      <h1>Our Team</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure tenetur
+        ducimus laboriosam eaque recusandae tempora enim aut, quae error eum
+        ipsum reiciendis temporibus dolore delectus ullam impedit cumque id.
+        Itaque?
+      </p>
+      <Link href="/">Home</Link>
+    </div>
+  )
+}
+
+export default TeamPage
