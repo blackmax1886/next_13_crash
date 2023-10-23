e52759c6243192b2b251a6f67477baa4046d1c2a
blackmax <blackmax1886@gmail.com>
2023-10-23 16:36:30 +0900

    style: poppins font for body

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,12 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { Poppins } from 'next/font/google'
 
+const poppins = Poppins({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+})
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -18,7 +23,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={poppins.className}>
+        <main className="container">{children}</main>
+      </body>
     </html>
   )
 }
