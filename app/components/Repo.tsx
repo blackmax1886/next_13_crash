76ba16928ff7be5d17bdb2d7085eeb5f45eb91ed
blackmax <blackmax1886@gmail.com>
2023-10-23 18:43:36 +0900

    Add: Repo components

---
+++ app/components/Repo.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { FaStar, FaCodeBranch, FaEye } from 'react-icons/fa'
+import { GithubRepo } from '../type/github'
+
+type RepoProps = {
+  name: string
+}
+
+async function fetchRepo(name: string): Promise<GithubRepo> {
+  const response = await fetch(
+    `https://api.github.com/repos/blackmax1886/${name}`
+  )
+  const repo = await response.json()
+
+  return repo
+}
+
+const Repo = async ({ name }: RepoProps) => {
+  const repo = await fetchRepo(name)
+  return (
+    <>
+      <h2>{repo.name}</h2>
+      <p>{repo.description}</p>
+      <div className="card-stats">
+        <div className="card-stat">
+          <FaStar />
+          <span>{repo.stargazers_count}</span>
+        </div>
+        <div className="card-stat">
+          <FaCodeBranch />
+          <span>{repo.forks_count}</span>
+        </div>
+        <div className="card-stat">
+          <FaEye />
+          <span>{repo.watchers_count}</span>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Repo
