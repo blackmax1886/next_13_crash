39ef557c250e079423f5f3dac4657ca10b266d44
blackmax <blackmax1886@gmail.com>
2023-10-23 12:00:38 +0900

    feat: cleanup template

---
+++ app/about/page.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>AboutPage</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit
+        molestiae ipsam, et aut consequatur ipsum voluptates quasi, quos
+        recusandae doloribus provident consequuntur amet nobis est voluptate
+        perferendis quaerat distinctio saepe dolores perspiciatis ex ab nostrum
+        eaque! Porro perspiciatis possimus, sed a quidem sunt sit doloremque
+        molestiae maiores blanditiis quasi quod.
+      </p>
+      <Link href="/">Home</Link>
+    </div>
+  )
+}
+
+export default AboutPage
