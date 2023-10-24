bf5f923bbef3d12b4a5bda28f539251c61f660d4
blackmax <blackmax1886@gmail.com>
2023-10-24 12:21:10 +0900

    Feat: individual loading with suspense

--- app/code/repos/[name]/page.tsx
+++ app/code/repos/[name]/page.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import React from 'react'
+import React, { Suspense } from 'react'
 
 import Repo from '@/app/components/Repo'
 import RepoDirs from '@/app/components/RepoDirs'
@@ -16,8 +16,12 @@ const RepoPage = ({ params: { name } }: RepoPageProps) => {
       <Link href="/code/repos" className="btn btn-back">
         Back To Repositories
       </Link>
-      <Repo name={name} />
-      <RepoDirs name={name} />
+      <Suspense fallback={<div>Loading repo...</div>}>
+        <Repo name={name} />
+      </Suspense>
+      <Suspense fallback={<div>Loading directories...</div>}>
+        <RepoDirs name={name} />
+      </Suspense>
     </div>
   )
 }
