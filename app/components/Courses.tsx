b755c505db1819cae4948f520f1ce3cb7ca43abd
blackmax <blackmax1886@gmail.com>
2023-10-24 17:42:31 +0900

    Refactor: lift-up fetching courses data

--- app/components/Courses.tsx
+++ app/components/Courses.tsx
@@ -3,14 +3,7 @@ import React from 'react'
 
 import Course from '../type/course'
 
-async function fetchCourses() {
-  const response = await fetch('http://localhost:3000/api/courses')
-  const courses = await response.json()
-  return courses
-}
-
-const Courses = async () => {
-  const courses = await fetchCourses()
+const Courses = async ({ courses }: { courses: Course[] }) => {
   return (
     <div className="courses">
       {courses.map((course: Course) => (
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,34 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+
 import Courses from './components/Courses'
+import LoadingPage from './loading'
 
 const HomePage = () => {
+  const [courses, setCourses] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      const res = await fetch('/api/courses')
+      const data = await res.json()
+      setCourses(data)
+      setLoading(false)
+    }
+
+    fetchCourses()
+  }, [])
+
+  if (loading) {
+    return <LoadingPage />
+  }
+
   return (
-    <div>
+    <>
       <h1>Welcome To My Media</h1>
-      <Courses></Courses>
-    </div>
+      <Courses courses={courses} />
+    </>
   )
 }
 
